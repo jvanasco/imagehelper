44ee8bba2fb9dc0be14cf0f71db3c94638f686e8
jonathan vanasco <jonathan@2xlp.com>
2013-08-01 19:01:36 -0400

    updated setup.py with 0.1.0 rc 3

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     name='imagehelper',
     author='Jonathan Vanasco',
     author_email='jonathan@findmeon.com',
-    version='0.1.0..rc2',
+    version='0.1.0..rc3',
     url='http://github.com/jvanasco/imagehelper',
     packages=['imagehelper'],
     description='simple utilites for image resizing and uploading and stuff like that',
