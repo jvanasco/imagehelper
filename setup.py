33c6a10e659fdddeb64cdcfcf0d2bb5249103eb6
jonathan vanasco <jonathan@2xlp.com>
2020-10-15 14:39:01 -0400

    action fun

--- setup.py
+++ setup.py
@@ -20,22 +20,18 @@ requires = [
 tests_require = [
     "boto",
     "requests",
-]
-testing_extras = tests_require + [
     "pytest",
 ]
+testing_extras = tests_require + []
 
 setup(
     name="imagehelper",
-    author="Jonathan Vanasco",
-    author_email="jonathan@findmeon.com",
     version=VERSION,
     url="http://github.com/jvanasco/imagehelper",
-    packages=find_packages(exclude=("tests",)),
+    author="Jonathan Vanasco",
+    author_email="jonathan@findmeon.com",
     description="simple utilites for image resizing and uploading and stuff like that",
     long_description="""The `imagehelper` package offers a simple interface for image resizing, optimizing and uploading. Core image resizing operations are handled by the `Pillow` (PIL) package; S3 uploading is handled by `boto`, and there are hooks for optimizing the images with the commandline tools: `advpng`,  `gifsicle`, `jpegtran`, `jpegoptim`, `optipng` and `pngcrush.`""",
-    zip_safe=False,
-    test_suite="tests",
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 2",
@@ -44,9 +40,14 @@ setup(
         "Topic :: Multimedia :: Graphics :: Graphics Conversion",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
+    py_modules=["imagehelper"],
+    license="BSD",
+    packages=find_packages(exclude=("tests",)),
+    zip_safe=False,
     install_requires=requires,
     tests_require=tests_require,
-    extras={
+    extras_require={
         "testing": testing_extras,
     },
+    test_suite="tests",
 )
